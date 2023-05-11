6b129e47a7d007b4edc085f311eb0b4c5bf1fb97
richbharti154 <116936700+richbharti154@users.noreply.github.com>
2023-05-11 12:28:30 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Game-using-function
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-davu1i)
---
+++ src/Game.js
@@ -0,0 +1,28 @@
+import React,{useState} from 'react'
+import styles from './Game.module.css'
+function Game() {
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(false);
+
+  const handleClick = () => {
+    if (!gameOver) {
+      setScore(score + 1);
+      if (score >= 10) {
+        setGameOver(true);
+      }
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>My Game</h1>
+      <p className={styles.score}>Score: {score}</p>
+      {gameOver && <p className={styles.gameOver}>Game Over!</p>}
+      <button className={styles.button} onClick={handleClick}>
+        Click Me!
+      </button>
+    </div>
+  );
+}
+
+export default Game;
